953e7fc97bc1f0cce040b2d71ca0934e7a8674ea
Adam Recvlohe <adam.recvlohe@pm.me>
2021-09-17 19:17:57 -0500

    Add Tailwind, TS, & other misc changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,8 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the Preact renderer to support Preact JSX components.
-  renderers: ['@astrojs/renderer-preact'],
+  renderers: ["@astrojs/renderer-preact"],
+  devOptions: {
+    tailwindConfig: "./tailwind.config.js",
+  },
 });
