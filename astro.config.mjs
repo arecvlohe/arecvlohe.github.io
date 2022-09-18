b31cc10149f4cac2e85e5b0e467f364f15db437f
Adam Recvlohe <adam.recvlohe@pm.me>
2022-09-18 10:24:21 -0500

    Add sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [preact(), tailwind()],
+  integrations: [preact(), tailwind(), sitemap()],
   site: process.env.BUILD_GITLAB
-    ? "https://arecvlohe.gitlab.com"
+    ? "https://adamrecvlohe.com"
     : "https://arecvlohe.github.io",
   outDir: process.env.BUILD_GITLAB ? "public" : undefined,
 });
