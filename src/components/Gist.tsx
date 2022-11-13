04d8515e5f145a2e8437917da7dd42d5fb48ede8
Adam Recvlohe <adam.recvlohe@pm.me>
2022-11-12 23:05:09 -0600

    Show when site was last updated

--- src/components/Gist.tsx
+++
@@ -1,5 +0,0 @@
-export function Gist({ src }: { src: string }) {
-  return (
-    <iframe frameborder="0" scrolling="no" width="100%" src={src}></iframe>
-  );
-}
--- utils/functions.ts
+++ utils/functions.ts
@@ -1,5 +1,32 @@
+import * as git from "git-last-commit";
+
 export const mkAssetsPath = (path: string): string => {
   return process.env.NODE_ENV === "production"
     ? `/assets/${path}`
     : `/static/assets/${path}`;
+};
+
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+} as const;
+
+export const lastUpdated = (): Promise<string> => {
+  return new Promise((res, rej) => {
+    git.getLastCommit((err, commit) => {
+      if (err) {
+        return rej(`Failed to get last commit with: ${err.message}`);
+      }
+
+      try {
+        const unixTimestamp = parseInt(commit.authoredOn, 10) * 1000;
+        const date = new Date(unixTimestamp);
+        res(date.toLocaleDateString("en", options));
+      } catch (e) {
+        return rej(`Failed to parse last commit with: ${e}`);
+      }
+    });
+  });
 };
